c5e61a720a351aa1839817c4c506a89dbcb5729c
SWETHA-0945 <swethamk45@gmail.com>
2025-01-21 16:26:43 +0530

    updated

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import {assets} from '../assets/assets'
-const Navbar = () => {
-  return (
-    <div>
-        <div>
-            <img src={assets.logo}/>
-        </div>
-    </div>
-  )
-}
+import React from 'react';
+import { assets } from '../assets/assets';
+import { useClerk ,useUser, UserButton} from '@clerk/clerk-react';
+import { Link } from 'react-router-dom';
 
-export default Navbar
+const Navbar = () => {
+  const { openSignIn } = useClerk();
+  const {user} =useUser()
+
+  return (
+    <div className="shadow py-4">
+      <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
+        <img src={assets.logo} alt="Logo" />
+        {
+          user
+          ?<div>
+            <Link to={'/applications'}>Applied Jobs</Link>
+            <p>|</p>
+            <p>Hi, {user.firstName+" "+user.lastName}</p>
+            <UserButton/>
+          </div>
+          :<div className="flex gap-4 max-sm:text-xs">
+          <button  className="text-gray-600">
+            Recruiter Login
+          </button>
+          <button onClick={() => openSignIn()} className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full">
+            Login
+          </button>
+        </div>
+        }
+        
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
