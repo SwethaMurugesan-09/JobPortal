3bdcb57677545425c539cb041272486c645bd5a2
SWETHA-0945 <swethamk45@gmail.com>
2025-01-25 00:26:26 +0530

    updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
+import JobListing from '../Components/JobListing'
+import Footer from '../Components/Footer'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <Hero/>
+        <JobListing/>
+        <Footer/>
     </div>
   )
 }
