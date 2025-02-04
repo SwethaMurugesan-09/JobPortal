3b0abe06b67fdd5a64372393630081cc998617ca
SWETHA-0945 <swethamk45@gmail.com>
2025-02-04 18:53:52 +0530

    updated

---
+++ server/config/instrument.js
@@ -0,0 +1,28 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+
+import * as Sentry from "@sentry/node"
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://acac892ab558d2b7784d07848ff2db24@o4508759295000576.ingest.us.sentry.io/4508759301881856",
+  integrations: [
+    nodeProfilingIntegration(),
+    Sentry.mongooseIntegration()
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+});
+// Manually call startProfiler and stopProfiler
+// to profile the code in between
+Sentry.profiler.startProfiler();
+
+// Starts a transaction that will also be profiled
+Sentry.startSpan({
+  name: "My First Transaction",
+}, () => {
+  // the code executing inside the transaction will be wrapped in a span and profiled
+});
+
+// Calls to stopProfiling are optional - if you don't stop the profiler, it will keep profiling
+// your application until the process exits or stopProfiling is called.
+Sentry.profiler.stopProfiler();
--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js'
+import './config/instrument.js'
+import * as Sentry from "@sentry/node";
 
 const app=express()
+await connectDB()
 
 app.use(cors())
+
+//whenever we get the response then the response will be passed using json method
+app.use(express.json())
+
+app.get('/',(req,res)=>res.send("Api working"))
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+  
+const PORT=process.env.PORT || 5000
+Sentry.setupExpressErrorHandler(app);
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+})
