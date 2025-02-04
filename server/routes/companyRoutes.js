e39770665f9860da3aa739d2309fb8aa927e477f
SWETHA-0945 <swethamk45@gmail.com>
2025-02-05 00:52:02 +0530

    updated

---
+++ server/routes/companyRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import { changeJobApllicationsStatus, changeVisibility, getCompanyData, getCompanyJobApplicants, getCompanyPostedJobs, loginCompany, postJob, registerCompany } from '../controller/companyController'
+import upload from '../config/multer'
+
+const router =express.Router()
+
+router.post('/register',upload.single('image') ,registerCompany)
+
+router.post('/login',loginCompany)
+
+router.get('/company',getCompanyData)
+
+router.post('/post-job',postJob)
+
+router.get('/applicants',getCompanyJobApplicants)
+
+router.get('/list-jobs',getCompanyPostedJobs)
+
+router.post('/change-status',changeJobApllicationsStatus)
+
+router.post('/change-visibility',changeVisibility)
+
+export default router
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import connectDB from './config/db.js'
 import './config/instrument.js'
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controller/webhooks.js'
-
+import companyRoutes from './routes/companyRoutes.js'
 const app=express()
 await connectDB()
 
@@ -21,6 +21,10 @@ app.get("/debug-sentry", function mainHandler(req, res) {
   
 
 app.post('/webhooks',clerkWebhooks)
+
+app.use('/api/company',companyRoutes)
+
+
 const PORT=process.env.PORT || 5000
 Sentry.setupExpressErrorHandler(app);
 
