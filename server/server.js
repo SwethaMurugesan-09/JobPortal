55cff751506754a38bfbd3a6c50e4d78ef317cea
SWETHA-0945 <swethamk45@gmail.com>
2025-02-04 21:15:46 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/db.js'
 import './config/instrument.js'
 import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from './controller/webhooks.js'
 
 const app=express()
 await connectDB()
@@ -18,6 +19,8 @@ app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
   });
   
+
+app.post('/webhooks',clerkWebhooks)
 const PORT=process.env.PORT || 5000
 Sentry.setupExpressErrorHandler(app);
 
