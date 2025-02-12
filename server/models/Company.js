02247a544d499d835b6d97ca500886985c0c9061
SWETHA-0945 <swethamk45@gmail.com>
2025-02-12 19:18:10 +0530

    updated

--- server/models/Company.js
+++ server/models/Company.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const companysSchema = new mongoose.Schema({
+const companySchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     image: { type: String, required: true },
     password: { type: String, required: true }, 
 })
 
-const Companys = mongoose.model('Companys',companysSchema)
+const Company = mongoose.model('Company',companySchema)
 
-export default Companys
+export default Company
